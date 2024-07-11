29267b3691f9de51464989b81e3b1e0fc970c1f4
sanjay-546 <sanjays22278@gmail.com>
2024-07-11 20:25:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var on_button=document.getElementById("on");
+var off_button=document.getElementById("off");
+var image=document.getElementById("bulb");
+function bulb_on() {
+    image.src="./image/bulb_on.jpeg"
+}
+function bulb_off() {
+    image.src="./image/bulb_off.jpeg"
+}
